0fe78982593dd326b907fc3eba38f38bf6685e43
Pawan Eswaran <pawaneswaran19012006@gmail.com>
2026-08-23 01:31:49 +0530

    added comments to webcam.py

--- webcam.py
+++ webcam.py
@@ -6,17 +6,17 @@ model = YOLO("yolo11n.pt")
 cap = cv2.VideoCapture(0)
 
 while True:
-    success, frame = cap.read()
+    success, frame = cap.read() #read the frames one by one from the camera - it is used through opencv
 
     if not success:
         print("Failed to read frame")
         break
 
-    results = model(frame)
+    results = model(frame) #run the inference on that frame to detect the object - yolo
 
-    annotated_frame = results[0].plot()
+    annotated_frame = results[0].plot() #draw the results lively on the video itself
 
-    cv2.imshow("YOLO Camera", annotated_frame)
+    cv2.imshow("YOLO Camera", annotated_frame) # display that image along with the resultant(boundary drawn)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
